96be623f3a026ea3dca2ad1c0287164af8ab3690
Kentrell <lighterdark2607@gmail.com>
2025-07-16 08:35:05 -0400

    remove extra template

--- data/templateOptions.js
+++ data/templateOptions.js
@@ -1,14 +1,8 @@
 module.exports = [
   {
-    id: 'profile_twilight',
-    name: 'Twilight Theme',
-    price: 2,
-    file: 'profile_twilight.png'
-  },
-  {
     id: 'profile_blue',
     name: 'Blueberry Sunrise',
-    price: 2,
+    price: 5,
     file: 'profile_blue.png'
   }
 ];
