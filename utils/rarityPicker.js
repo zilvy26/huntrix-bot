41aef6089d73a1f4107a35048802b15b569015eb
Kentrell <lighterdark2607@gmail.com>
2025-07-22 22:11:22 -0400

    gacha changes

--- utils/rarityPicker.js
+++ utils/rarityPicker.js
@@ -3,10 +3,10 @@ const parseRarity = require('./parseRarity'); // we'll use the parser too
 // These can stay in "label" form for config, but will be converted
 const rawRarities = {
   '1S': 36.75,
-  '2S': 28.25,
-  '3S': 20,
+  '2S': 28.5,
+  '3S': 20.25,
   '4S': 11.5,
-  '5S': 3.5,
+  '5S': 3,
 };
 
 /**
