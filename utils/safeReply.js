dcee698f5f7b46c880d31b76f555f9f24895b1f8
Kentrell <lighterdark2607@gmail.com>
2025-07-23 16:01:53 -0400

    changes x5

--- utils/safeReply.js
+++ utils/safeReply.js
@@ -1,12 +1,14 @@
 module.exports = async function safeReply(interaction, options = {}) {
   try {
+    // Remove ephemeral if it's editReply
+    const cleanOptions = { ...options };
     if (interaction.replied || interaction.deferred) {
-      return await interaction.editReply(options);
+      delete cleanOptions.ephemeral;
+      return await interaction.editReply(cleanOptions);
     } else {
-      return await interaction.reply(options);
+      return await interaction.reply(cleanOptions);
     }
   } catch (err) {
     console.warn('Failed to reply to interaction:', err.message);
-    return null;
   }
 };
