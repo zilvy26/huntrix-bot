6a14db868e37c458610cacf1ccc26ef3f2b7c833
Kentrell <lighterdark2607@gmail.com>
2025-08-04 08:48:57 -0400

    hi

--- models/UserRecord.js
+++ models/UserRecord.js
@@ -5,7 +5,7 @@ const logSchema = new mongoose.Schema({
   type: { type: String, required: true },
   targetId: { type: String },
   detail: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, index: { expires: '30d' } }
+  createdAt: { type: Date, default: Date.now, index: { expires: '60' } }
 });
 
 module.exports = mongoose.model('UserRecord', logSchema);
