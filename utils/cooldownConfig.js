a2b854051fb57cdb7eadf7f4c5154e93f77d7487
Kentrell <lighterdark2607@gmail.com>
2025-08-15 17:23:37 -0400

    cd reduce

--- utils/cooldownConfig.js
+++ utils/cooldownConfig.js
@@ -2,12 +2,12 @@
 
 module.exports = {
   Pull: {
-    default: 75 * 1000,
+    default: 90 * 1000,
     reductions: [
-      { id: '1387230787929243780', percent: 20 }, // Booster
+      { id: '1387230787929243780', percent: 15 }, // Booster
       { id: '1394845122180677662', percent: 20 }, // Maknae
       { id: '1394846623971938465', percent: 25 }, // Visual
-      { id: '1394847239557615666', percent: 30 }, // Leader
+      { id: '1394847239557615666', percent: 35 }, // Leader
       { id: '1394448143206322267', percent: 10 } // Huntrixbot
     ]
   },       
@@ -15,7 +15,7 @@ module.exports = {
   Perform: {
     default: 30 * 60 * 1000,
     reductions: [
-      { id: '1394847239557615666', percent: 20 }, // Leader
+      { id: '1394847239557615666', percent: 25 }, // Leader
     ]
   },
   Rehearsal:  45 * 60 * 1000,
@@ -23,17 +23,17 @@ module.exports = {
     default: 1.25 * 60 * 60 * 1000,
     reductions: [
       { id: '1394448143206322267', percent: 10 }, // Huntrixbot
-      { id: '1387230787929243780', percent: 20 }, // Booster
+      { id: '1387230787929243780', percent: 15 }, // Booster
       { id: '1394845122180677662', percent: 20 }, // Maknae
       { id: '1394846623971938465', percent: 25 }, // Visual
-      { id: '1394847239557615666', percent: 30 }, // Leader
+      { id: '1394847239557615666', percent: 35 }, // Leader
     ]
   },
   Battle: {
     default: 20 * 60 * 1000,
     reductions: [
       { id: '1394846623971938465', percent: 15 }, // Visual
-      { id: '1394847239557615666', percent: 20 }, // Leader
+      { id: '1394847239557615666', percent: 25 }, // Leader
     ]
   },
   Vote: 12 * 60 * 60 * 1000,
