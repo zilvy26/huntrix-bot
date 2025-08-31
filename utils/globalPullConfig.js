50d88c22ce013736056d3ea0aced432af9c192a6
Kentrell <lighterdark2607@gmail.com>
2025-08-31 08:15:06 -0400

    po

--- utils/globalPullConfig.js
+++ utils/globalPullConfig.js
@@ -11,7 +11,7 @@ const RARITY_WEIGHTS = {
 
 // Hardcoded multipliers for eras (always lowercased keys!)
 const ERA_MULTIPLIERS = {
-  'vir25': 0.38,
+  'vir25': 0.32,
 };
 
 // Hardcoded multipliers for specific card codes (always lowercased keys!)
